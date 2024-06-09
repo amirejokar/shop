a3b725d6788969d8363416908cc40ecce3dbe2a2
amirejokar <amirjowkar2@gmail.com>
2024-06-09 11:58:46 +0330

    folder structiring and app.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import Topbar from './Components/Topbar'
+
+export default function App() {
+  return (
+    <div>
+        <Topbar/>
+    </div>
+  )
+}
