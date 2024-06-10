5a1bf5638d68fa3fccf33dc8c7d65098de796f3b
amirejokar <amirjowkar2@gmail.com>
2024-06-10 14:01:12 +0330

    fix navbar in desktop size

--- src/Components/Topbar/index.jsx
+++ src/Components/Topbar/index.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
+import topimage from "./top-mobile.jpg";
+import "./index.css";
 
 export default function Topbar() {
   return (
     <div>
-      <div className="header-img">
-        <img src="../../../public/image/top-mobile.jpg" alt="khoshkel khanom" />
+      <div>
+        <img
+          className="header-img col-12"
+          src={topimage}
+          alt="khoshkel khanom"
+        />
       </div>
-      <div className="nabar">
-        <div className="navbar-left">online shop name </div>
-        <div className="navbar-center">
-          <ul>
-            <li>all products</li>
-            <li>product 1</li>
+      <div className="nabar col-10 m-auto ">
+        <div className="navbar-left col-2 m-auto">online shop name </div>
+        <div className="navbar-center col-8 m-auto ">
+          <ul className="navbar-center-content m-aut">
+            <li >all products</li>
+            <li className="active-item">product 1</li>
             <li>product 2</li>
             <li>product 3</li>
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
