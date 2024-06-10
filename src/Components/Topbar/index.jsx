1d752eb6503b3add529bd07d89fd8b27a91dd453
amirejokar <amirjowkar2@gmail.com>
2024-06-10 12:58:49 +0330

    fix botstrap and html of topbar

--- src/Components/Topbar/index.jsx
+++ src/Components/Topbar/index.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Topbar() {
   return (
-    <div>index</div>
+    <div>
+      <div className="header-img">
+        <img src="../../../public/image/top-mobile.jpg" alt="khoshkel khanom" />
+      </div>
+      <div className="nabar">
+        <div className="navbar-left">online shop name </div>
+        <div className="navbar-center">
+          <ul>
+            <li>all products</li>
+            <li>product 1</li>
+            <li>product 2</li>
+            <li>product 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
