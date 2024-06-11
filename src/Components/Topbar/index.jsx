a983db9200793d4690a5e2d42c51cea0a3414181
amirejokar <amirjowkar2@gmail.com>
2024-06-11 11:13:18 +0330

    fix image

--- src/Components/Topbar/index.jsx
+++ src/Components/Topbar/index.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import topimage from "./top-mobile.jpg";
+import topimage from "./home.jpg";
 import "./index.css";
 
 export default function Topbar() {
   return (
-    <div>
-      <div>
-        <img
-          className="header-img col-12"
-          src={topimage}
-          alt="khoshkel khanom"
-        />
-      </div>
-      <div className="nabar col-10 m-auto ">
+    <div className="d-flex justify-content-center ">
+      <div className="nabar col-10 ">
         <div className="navbar-left col-2 m-auto">online shop name </div>
         <div className="navbar-center col-8 m-auto ">
           <ul className="navbar-center-content m-aut">
-            <li >all products</li>
+            <li>all products</li>
             <li className="active-item">product 1</li>
             <li>product 2</li>
             <li>product 3</li>
           </ul>
         </div>
+      </div>
+      <div className="mask col-12">
+        <img
+          className="header-img col-12"
+          src={topimage}
+          alt="khoshkel khanom"
+        />
       </div>
     </div>
   );
