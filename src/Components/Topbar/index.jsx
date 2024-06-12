fa81482b64d4fc018d41a58ee5f258123cfc7625
amirejokar <amirjowkar2@gmail.com>
2024-06-12 10:08:57 +0330

    install react icone

--- src/Components/Topbar/index.jsx
+++ src/Components/Topbar/index.jsx
@@ -6,7 +6,10 @@ export default function Topbar() {
   return (
     <div className="d-flex justify-content-center ">
       <div className="nabar col-10 ">
-        <div className="navbar-left col-lg-2 col-md-3 m-auto fs-lg-1 fs-md-2 fs-sm-3 fs-3">online shop name </div>
+        <div className="navbar-left col-lg-2 col-md-3 m-auto fs-lg-1 fs-md-2 fs-sm-3 fs-3">
+          online shop name 
+          <div></div>
+          </div>
         <div className="navbar-center col-lg-8 col-md-9 m-auto d-none d-lg-flex d-md-flex col-8 m-auto ">
           <ul className="navbar-center-content m-aut">
             <li>all products</li>
