34d9a0680cbb5d52c2fe551980768e9a5da229b6
amirejokar <amirjowkar2@gmail.com>
2024-06-16 14:17:31 +0330

    install matrial ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Topbar from './Components/Topbar'
+import Cards from './Components/Curds'
 
 export default function App() {
   return (
     <div>
         <Topbar/>
+        <Cards/>
     </div>
   )
 }
