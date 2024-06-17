ff83efe97334ee3add9343900f003262f65c9920
amirejokar <amirjowkar2@gmail.com>
2024-06-17 12:00:00 +0330

    fix card image in modile size

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 export default function Card({ img, txt }) {
   return (
-    <div className="card m-auto col-xl-2 col-lg-2 col-md-2  col-sm-3 col-5  mt-5 ">
+    <div className="card m-auto col-xl-2 col-lg-2 col-md-2  col-sm-2 col-5  mt-5 ">
       <div className="mask-card">
         <img src={img} className="card-img" alt="khanom khanomi " />
       </div>
