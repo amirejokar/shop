eb06cc1f531f192889c9501abfb72a208478834f
amirejokar <amirjowkar2@gmail.com>
2024-06-20 15:15:53 +0330

    fix image cardes in poster component

--- src/Components/Poster/index.jsx
+++ src/Components/Poster/index.jsx
@@ -1,23 +1,15 @@
 import React from "react";
+import "./index.css";
 import poster1 from "./poster1.jpg";
 import poster2 from "./poster2.jpg";
-import "./index.css";
 export default function Posters() {
   return (
-    <div className="poster">
-      <div className="poster-img col-6">
-        <div className="img-box">
-          <img src={poster1} alt="khoshkel khanom" />
-          <img src={poster2} alt="khoshkel khanom" />
-        </div>
+    <div class="container">
+      <div data-text="name" style={{ "--r": "-15" }} class="glass">
+        <img className="back-img" src={poster1} alt="khoshkel khanomi" />
       </div>
-      <div className="poster-text col-6">
-        <p className="txt">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-          temporibus dolor libero pariatur exercitationem? Eveniet, ipsum facere
-          est autem libero minima beatae atque maiores. Ratione delectus iure
-          facere dolore nulla?
-        </p>
+      <div data-text="name" style={{ "--r": "--r" }} class="glass">
+        <img className="back-img" src={poster2} alt="khoshkel khanomi" />
       </div>
     </div>
   );
