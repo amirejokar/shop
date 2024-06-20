4e485db9111479b55fd88a0c63baeca6f582c68e
amirejokar <amirjowkar2@gmail.com>
2024-06-20 14:54:57 +0330

    try to fix posters

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import Topbar from './Components/Topbar'
 import Cards from './Components/Cards'
+import Posters from './Components/Poster'
 export default function App() {
   return (
     <div>
         <Topbar/>
         <Cards/>
+        <Posters/>
     </div>
   )
 }
--- src/Components/Poster/index.jsx
+++ src/Components/Poster/index.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import poster1 from "./poster1.jpg";
 import poster2 from "./poster2.jpg";
-export default function Poster() {
+import "./index.css";
+export default function Posters() {
   return (
-    <div>
-      <div className="poster-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-        temporibus dolor libero pariatur exercitationem? Eveniet, ipsum facere
-        est autem libero minima beatae atque maiores. Ratione delectus iure
-        facere dolore nulla?
-      </div>
-      <div className="poster-img">
+    <div className="poster">
+      <div className="poster-img col-6">
         <div className="img-box">
           <img src={poster1} alt="khoshkel khanom" />
           <img src={poster2} alt="khoshkel khanom" />
         </div>
+      </div>
+      <div className="poster-text col-6">
+        <p className="txt">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+          temporibus dolor libero pariatur exercitationem? Eveniet, ipsum facere
+          est autem libero minima beatae atque maiores. Ratione delectus iure
+          facere dolore nulla?
+        </p>
       </div>
     </div>
   );
