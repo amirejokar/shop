c975e8caf4f9030078877b5a9913c53cc69c52dc
amirejokar <amirjowkar2@gmail.com>
2024-06-21 15:24:16 +0330

    fix poster container and text in desktop size

--- src/Components/Poster/index.jsx
+++ src/Components/Poster/index.jsx
@@ -4,12 +4,23 @@ import poster1 from "./poster1.jpg";
 import poster2 from "./poster2.jpg";
 export default function Posters() {
   return (
-    <div class="container">
-      <div data-text="name" style={{ "--r": "-15" }} class="glass">
-        <img className="back-img" src={poster1} alt="khoshkel khanomi" />
+    <div className="poster-section d-flex m-auto">
+      <div class="container">
+        <div data-text="name" style={{ "--r": "-15" }} class="glass">
+          <img className="back-img" src={poster1} alt="khoshkel khanomi" />
+        </div>
+        <div data-text="name" style={{ "--r": "--r" }} class="glass">
+          <img className="back-img" src={poster2} alt="khoshkel khanomi" />
+        </div>
       </div>
-      <div data-text="name" style={{ "--r": "--r" }} class="glass">
-        <img className="back-img" src={poster2} alt="khoshkel khanomi" />
+      <div className="poster-txt col-md-5 m-auto ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        mollitia odio optio autem incidunt laudantium animi ipsum eligendi sequi
+        nisi deleniti cupiditate vero repellendus voluptate alias, quasi, quis
+        porro architecto.
+        upiditate vero repellendus voluptate alias, quasi, quis
+        porro architecto.upiditate vero repellendus voluptate alias, quasi, quis
+        porro architecto.
       </div>
     </div>
   );
