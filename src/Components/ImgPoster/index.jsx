3c0a173becb8dbdf456d6e4e90192b5d509d041f
amirejokar <amirjowkar2@gmail.com>
2024-06-24 19:36:31 +0330

    fix text of image poster

--- src/Components/ImgPoster/index.jsx
+++ src/Components/ImgPoster/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import './index.css'
-import poster from './poster-final.jpg'
+import React from "react";
+import "./index.css";
+import poster from "./poster-final.jpg";
 
 export default function ImagePoster() {
   return (
-    <div className='img-poster'>
+    <div className="image-poster-box">
+      <div className="img-poster mask">
         <img src={poster} alt="khoshkele" />
+      </div>
+      <p className="txt-poster">lorem lorem lorem</p>
     </div>
-  )
+  );
 }
