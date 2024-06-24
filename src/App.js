d72f930ef2a836ad0eb3ba64c8a1ab349d089a95
amirejokar <amirjowkar2@gmail.com>
2024-06-24 18:48:16 +0330

    folder structuring of ImagePoster

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import Topbar from './Components/Topbar'
 import Cards from './Components/Cards'
 import Posters from './Components/Poster'
+import ImagePoster from './Components/ImgPoster'
 export default function App() {
   return (
     <div>
         <Topbar/>
         <Cards/>
         <Posters/>
+        <ImagePoster/>
     </div>
   )
 }
