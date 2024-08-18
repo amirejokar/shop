db4706708575cdf2bf3daacfc64824c4a90301ec
amirejokar <jokar@hero.best>
2024-08-18 17:07:46 +0330

    fix footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div className="content">
+            
+        </div>
+
+    </div>
+  )
+}
